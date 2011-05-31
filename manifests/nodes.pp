71b97cf6732e1cb16c8f64d52576eaacb0e03605
Erik LaBianca <erik.labianca@gmail.com>
2011-05-31 03:19:34 -0400

    add modules for java, solr, jenkins

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,6 +1,10 @@
 node 'vagrant-lucid64.vagrantup.com' inherits default {
 }
 
-node 'django-server' inherits default {
+node 'ip-10-245-218-240' inherits default {
   include django-stack
+  include jenkins
+  django-stack::app {"wiser_stg":
+  }
+  
 }
