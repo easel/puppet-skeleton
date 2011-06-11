42253c1c171c6a57cd8a353f7b64fda4e7b71959
Erik LaBianca <erik.labianca@gmail.com>
2011-06-10 23:08:04 -0400

    added example node definition to silence errors

---
+++ manifests/nodes/example.pp
@@ -0,0 +1,11 @@
+node 'vagrant' inherits default {
+    include django-stack
+    django-stack::app {"test_app":
+       vhost_name => "000-default",
+       server_name => "stg.vagrantup.local",
+       server_aliases => "*.stg.vagrantup.local, 
+    }
+}
+
+# vim modeline - have 'set modeline' and 'syntax on' in your ~/.vimrc.
+# vi:syntax=puppet:filetype=puppet:ts=8:sw=4:et:
