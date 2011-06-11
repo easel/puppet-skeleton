0fb0bcdf993ee0a29ec3c07e7f766784f24df926
Erik LaBianca <erik.labianca@gmail.com>
2011-06-10 23:04:37 -0400

    update to newer django-stack and dependencies

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,10 +1,4 @@
 node 'vagrant-lucid64.vagrantup.com' inherits default {
 }
 
-node 'ip-10-245-218-240' inherits default {
-  include django-stack
-  include jenkins
-  django-stack::app {"wiser_stg":
-  }
-  
-}
+import "nodes/*.pp"
